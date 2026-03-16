5805a470f882f907be0093db109bf00749c702e7
user1 <your-email@example.com>
2026-03-16 22:04:31 +0800

    编程式路由导航

---
+++ 编程式路由导航/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    <div class="row">
+      <div class="col-xs-offset-2 col-xs-8">
+        <Banner></Banner>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-2">
+        <div class="list-group">
+          <!-- <a class="list-group-item active" href="./about.html">About</a>
+          <a class="list-group-item" href="./home.html">Home</a> -->
+          <!--/使用router的标签：router-link，不能使用a标签，router-link会自动转为a标签 -->
+          <!-- to的作用为指定路径 -->
+          <!-- active-class属性能够根据点的位置，为他加上css属性 -->
+          <router-link class="list-group-item " to="/about" active-class="active">About</router-link>
+          <router-link class="list-group-item " to="/home" active-class="active">Home</router-link>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="panel">
+          <div class="panel-body">
+            <!-- router-view标签类似插槽的作用 -->
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Banner from './components/Banner.vue'
+export default {
+  name:'App',
+  data(){
+  },
+  components:{
+    Banner
+  }
+}
+</script>
+<style >
+  
+</style>
---
+++ 编程式路由导航/src/pages/message.vue
@@ -0,0 +1,61 @@
+<template>
+ <div>
+    <ul>
+        <li v-for="p in messagelist" :key='p.id'>
+        <!-- to对象式传送参数：:to=“{path：‘（写要进入到哪里）’，query：{写要传过去的参数}}” -->
+        <router-link 
+         :to="{
+            path:'/Home/message/detail',
+            query:{
+                id:p.id,
+                thing:p.thing
+            }
+         }">
+        {{p.thing}}
+        </router-link>&nbsp;&nbsp;
+        <button @click="pushlook(p)">push查看</button>
+        <button @click="replacelook(p)">replace查看</button>
+        </li>
+    </ul>
+    <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+    name:'MessageIt',
+    data(){
+        return{
+            messagelist:[
+                {id:'001',thing:'踢球'},
+                {id:'002',thing:'吃饭'},
+                {id:'003',thing:'喝水'}
+            ]
+        }
+    },
+    methods:{
+        pushlook(p){
+            //路由器router里自带的push，里面实现类似router-link标签的效果，需写一个配置项，path和query，写法和router-link一样
+          this.$router.push({
+           path:'/Home/message/detail',
+            query:{
+                id:p.id,
+                thing:p.thing
+            }
+          })
+        },
+        replacelook(p){
+            // //路由器router里自带的replace，里面实现类似router-link标签的效果，需写一个配置项，path和query，写法和router-link一样
+           this.$router.replace({
+           path:'/Home/message/detail',
+            query:{
+                id:p.id,
+                thing:p.thing
+            }
+          })
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
