735a6a68038366d1bb472c2c55ca4845e960add2
user1 <your-email@example.com>
2026-03-17 14:40:27 +0800

    独享，内路由守卫

---
+++ 独享，内路由守卫/src/pages/About.vue
@@ -0,0 +1,20 @@
+<template>
+   <h2>我是About的内容</h2>
+</template>
+<script>
+export default {
+    name:'AboutIt',
+    // //内路由配置
+    // // 进去前调用
+    // beforeRouteEnter(to, from, next){
+
+    // },
+    // // 进入后调用
+    // beforeRouteLeave (to, from, next) {
+       
+    // }
+}
+</script>
+<style lang="">
+    
+</style>
